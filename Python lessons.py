d1d9fe8ad6298252b669d738afc49a84b94a1cc7
SkeletonGuns <sergei.karlov2016@gmail.com>
2025-07-12 21:47:28 +0300

    Add files via upload

---
+++ Python lessons.py
@@ -0,0 +1,107 @@
+# a = 5
+# b = a
+# a = 9
+# print(a, b)
+#
+# a = b = c = 30
+# print(a, b, c)
+#
+# a = 1, 2
+# b = 'Helolo'
+# c = 1
+# c = 6.4
+# print(type(a), type(b), type(c))
+#
+# print(id(a), id(3))
+#
+# 4
+# 2 + 6.0
+# 8.0
+# 6 - 2
+# 4
+# 6 - 2.0
+# 4.0
+# 5 * 2
+# 10
+# 5 * 2.0
+# 10.0
+# 6 / 2
+# 3.0
+# 7 / 2
+# 3.5
+# 7 // 2
+# 3
+# -7 // 2
+# -4
+# 10 % 3
+# 1
+# -10 % 3
+# 2
+# -9 % 3
+# 0
+# 10 % (-3)
+# -2
+# -10 % -3
+# -1
+# 5 ** 2
+# 25
+# 2 > 1
+# True
+# 1 > 2
+# False
+# 1 < 2
+# True
+# 2 < 1
+# False
+# 2 >= 2
+# True
+# 2 >= 3
+# False
+# 2 == 2
+# True
+# 2 == 1
+# False
+# 2 != 2
+# False
+# 2 != 1
+# True
+# 2 == 2 and 1 == 1
+# True
+# True and 2 == 2 and False
+# False
+# 1 > 14 or 1 == 1
+# True
+# False or 2 >= 1 or False
+# True
+# not True
+# False
+# not True or 2 >= 1 or False
+# True
+# True or 2 >= 1 or False
+# True
+# not (True or 2 >= 1 or False)
+# False
+
+# if 2 > 2:
+#     print(1321)
+#     print(2 + 1)
+# elif 3 < 1:
+#     print(7)
+# elif 3 < 3:
+#     print(3 + 3)
+# else:
+#     print("Mistake")
+
+# -------------------------------------------------
+# num = int(input('Введите число: '))
+#
+# print(num)
+# print(type(num))
+#
+# print('13')
+#
+# print(type(str(346345.244234)))
+#
+# str()
+# print(int('Hello'))
+# float()
